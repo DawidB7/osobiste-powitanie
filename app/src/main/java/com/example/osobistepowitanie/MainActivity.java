97bedff1cb4965224fbc3c516edb5027ee394a7d
DawidB7 <podlasieauuau@gmail.com>
2025-04-25 11:16:32 +0200

    pozczatek

---
+++ app/src/main/java/com/example/osobistepowitanie/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.osobistepowitanie;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView
+
+}
